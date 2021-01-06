77b9301854bba8640b6ff7522497fa84e2654037
Alejandro Aragón <ar17092@ues.edu.sv>
2021-01-06 14:35:44 -0600

    Creando las rutas para los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./vistas/login/login.component";
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+import { SignupComponent } from './vistas/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo:'login',pathMatch:'full'
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+  {
+    path:'dashboard', component:DashboardComponent
+  },
+  {
+    path:'signup', component:SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const components = [LoginComponent,DashboardComponent,SignupComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,components } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
-import { LoginComponent } from './vistas/login/login.component';
-import { DashboardComponent } from './vistas/dashboard/dashboard.component';
-import { SignupComponent } from './vistas/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
-    DashboardComponent,
-    SignupComponent
+    components
   ],
   imports: [
     BrowserModule,
